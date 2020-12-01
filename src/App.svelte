bced34ef4f1481f0f69024e789ea1914b71c6c8c
Holger H <holgheiss@gmail.com>
2020-12-01 21:37:45 +0100

    menu starter

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,16 @@
 	import LayerContent from './LayerContent.svelte'
 	import LayerLogo from './LayerLogo.svelte'
 	import Layer3D from './Layer3D.svelte'
+	import LayerMenu from './LayerMenu.svelte'
+	
+	import GlobalStyle from './GlobalStyle.svelte' // import global styles
 </script>
 
 <main>
 	<LayerBg />
 	<LayerContent />
 	<Layer3D />
+	<LayerMenu />
 	<LayerLogo />
 </main>
 
---
+++ src/LayerMenu.svelte
@@ -0,0 +1,77 @@
+<script>
+  let isMenuActive = false
+
+  const onMenu = () => { isMenuActive = !isMenuActive }
+
+</script>
+
+<div class="toggler">
+  <div class="toggle-menu" on:click={onMenu} ></div>
+  <div class="toggle-silence"></div>
+</div>
+
+<nav class={ isMenuActive ? "active" : ""}>
+  <ol>
+    <li>action</li>
+    <li>action</li>
+    <li>action</li>
+    <li>action</li>
+  </ol>
+</nav>
+
+<style>
+
+  .toggler {
+    position: fixed;
+    left: 20px;
+    top: 20px;
+    z-index: 10;
+    mix-blend-mode: difference;
+  }
+
+  [class^="toggle-"] {
+    height: 50px;
+    border-radius: 25px;
+    display: inline-block;
+    background-color: #00FF94;
+    filter: blur(10px);
+    user-select: none;
+  }
+
+  [class^="toggle-"]:hover {
+    cursor: pointer;
+  }
+
+  .toggle-menu {
+    width: 200px;
+  }
+
+
+  .toggle-silence {
+    margin-left: 50px;
+    width: 50px;
+  }
+
+  nav:not(.active) {
+    display: none;
+  }
+
+  nav {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    pointer-events: none;
+    background-color: rgba(0,255,0,0.5);  
+    user-select: none;
+  }
+
+  nav:not(.active) {
+    display: none;
+  }
+
+  li {
+    pointer-events: all;
+  }
+</style>
