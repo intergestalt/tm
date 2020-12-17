b521c434090241d5b443a34e36a1fde1acb41a30
Holger H <holgheiss@gmail.com>
2020-12-17 09:42:00 +0100

    small changes to logo lines

--- src/Logo.svelte
+++ src/Logo.svelte
@@ -4,19 +4,48 @@
   export let onClick;
 
   let initialDeg = 0;
-  
+
+  const centerX = 135;
+
+
+  /*
   const svgRotateOffsetDeg = 0 // 135
+
+  const leftAnimConfig = {
+    offsetX1: centerX - 189.6
+  }*/
 
   onMount(async () => {
     initialDeg = svgRotateOffsetDeg + new Date().getSeconds() * 6;
-  })
+
+  /*
+    let frame = requestAnimationFrame(loop);
+    let framecounter = 0;
+
+    function loop(t) {
+      frame = requestAnimationFrame(loop);
+
+      framecounter++
+    }
+
+    return () => {
+      cancelAnimationFrame(frame);
+    };
+  */
+
+  });
+
+
+
+
+
 
 </script>
 
 <svg on:click={onClick} style="--start-deg: {initialDeg}deg; --end-deg: {initialDeg+360}deg;" width="266" height="266" viewBox="0 0 266 266" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M135.943,37.843L136.423,223.418" style="fill:none;fill-rule:nonzero;stroke:black;stroke-width:7.65px;"/>
-  <path class="left" d="M189.647,88.404L45.079,143.614" style="fill:none;fill-rule:nonzero;stroke:black;stroke-width:7.65px;"/>
-  <path class="right" d="M217.207,153.183L94.079,179.192" style="fill:none;fill-rule:nonzero;stroke:black;stroke-width:7.65px;"/>
+  <path stroke-linecap="round" d="M135.943,37.843L136.423,223.418" style="fill:none;fill-rule:nonzero;stroke:black;stroke-width:7.65px;"/>
+  <path stroke-linecap="round" class="left" d="M189.647,88.404L45.079,143.614" style="fill:none;fill-rule:nonzero;stroke:black;stroke-width:7.65px;"/>
+  <path stroke-linecap="round" class="right" d="M217.207,153.183L94.079,179.192" style="fill:none;fill-rule:nonzero;stroke:black;stroke-width:7.65px;"/>
 </svg>
 
 <style>
@@ -24,7 +53,7 @@
     /*will-change: transform;*/
     width: 100%;
     height: 100%;
-    animation-name: clock;
+    /*animation-name: clock;*/
     animation-duration: 60s;
     animation-iteration-count: infinite;
     animation-timing-function: linear;
@@ -34,7 +63,7 @@
     /*will-change: transform;*/
     transform-origin: 50% 40%; 
     animation-name: rotate;
-    animation-duration: 10s;
+    animation-duration: 8s;
     animation-iteration-count: infinite;
     animation-timing-function: linear;
   }
