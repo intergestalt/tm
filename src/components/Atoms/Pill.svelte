5e2c6a6e17d68d91575bb734bc1a07ef1793740a
Holger H <holgheiss@gmail.com>
2021-01-18 11:22:19 +0100

    add Pill

---
+++ src/components/Atoms/Pill.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { Link } from 'svelte-navigator'
+
+  export let title, link
+</script>
+
+<div class="container">
+  {#if link}
+    <Link to={link}>{ title }</Link>
+  {:else}
+    { title }
+  {/if}
+</div>
+
+<style lang="scss">
+  .container {
+    &, a {
+      @include typo-grotesk-text-18;
+    }
+    @include box-shadow;
+    display: inline;
+
+    padding: 5px 15px;
+    height: 34px;
+    border-radius: 20px;
+  }
+
+</style>
